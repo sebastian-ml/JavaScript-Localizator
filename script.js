bf33684371cc76d38453f8a627e8c1266f6ac034
sebastian-ml <sebastian2549@wp.pl>
2020-10-10 20:09:51 +0200

    Add location share denied popup
    Popup displays if user doesn't allow to share location. User can turn off the popup

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const figure = document.getElementsByClassName('figure')[0];
+
 // Initialize and add the map
 function initMap(position) {
+    // Hide the pending gif
+    figure.style.display = 'none';
+
     // Get current user coordinates
     const lat = position.coords.latitude;
     const long = position.coords.longitude;
@@ -22,17 +27,40 @@ const mapContainer = document.getElementById('map');
 const btn = document.getElementsByClassName('btn')[0];
 btn.addEventListener('click', displayUserLocation);
 
-
+// Show user location on map
 function displayUserLocation() {
     btn.style.display = 'none';
     if('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(initMap,
-            // Hide the button and show alert if user has denied lcoation promt
-            () => {
-                alert('You have to accept location prompt. Share your location and try again. \nFor help visit: https://support.google.com/chrome/answer/142065?hl=en');
-                btn.style.display = 'block';
-            })
+        // Show a pending gif
+        figure.style.display = 'flex';
+
+        // Get user coordinates
+        navigator.geolocation.getCurrentPosition(initMap, locationDenied);
     } else {
         alert('There is no geolocation in your device');
     }
 }
+
+const alertBox = document.getElementsByClassName('alert-box')[0];
+const alertBoxExit = alertBox.getElementsByClassName('exit-shape')[0];
+alertBoxExit.addEventListener('click', HideBox);
+
+function locationDenied() {
+    // Style the site properly if user has denied location share
+    figure.style.display = 'none';
+    btn.style.display = 'block';
+    alertBox.style.display = 'flex';
+    alertBox.style.opacity = '1';
+}
+
+// Hide the box if user clicks the exit button
+function HideBox() {
+    // Opacity transition is set to 600ms in css
+    alertBox.style.opacity = "0";
+
+    // Hide the div after 600ms (the same amount of milliseconds it takes to fade out)
+    setTimeout(() => {
+        alertBox.style.display = "none";
+        }, 600);
+}
+
